908fb340c9ea77cbc63322d7217da47b8aaf5139
gus <gustavo.rodrigues@cwi.com.br>
2021-02-05 19:10:38 -0300

    adicionando exercicio

---
+++ __tests__/series.test.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+import {
+  verificarSeAtorEstaEmSeriado,
+  filtarPorAnoERetornarNome,
+  calcularMediaTotalDeEpisodios,
+  agruparTituloDasSeriesPorPropriedade,
+} from '../src/metodos'
+
+
+describe('Exemplo de testes', () => {
+  it('Valor importado deve ser true', () => {
+    expect(true).toBeTruthy()
+  })
+})
---
+++ src/metodos.js
@@ -0,0 +1,15 @@
+export function filtarPorAnoERetornarNome(series, ano) {
+  return null
+}
+
+export function verificarSeAtorEstaEmSeriado(series, nomeAtor) {
+  return null
+}
+
+export function calcularMediaTotalDeEpisodios(series) {
+  return null
+}
+
+export function agruparTituloDasSeriesPorPropriedade(series, propriedade) {
+  return null
+}
